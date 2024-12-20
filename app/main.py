a064e1ea6e1c3cc7077fe490d3f0ff8aafb63bd5
dmitry-sol <dmitry-sol@mail.ru>
2024-12-21 06:02:39 +1100

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.routers import user, task
+
+
+api = FastAPI()
+
+api.include_router(user.router)
+api.include_router(task.router)
+
+@api.get('/')
+async def root():
+    return {"message": "Welcome to Taskmanager"}
+
+
+
+# uvicorn app.main:api --reload
---
+++ app/routers/task.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/task', tags=['task'])
+
+@router.get('/')
+async def all_tasks():
+    pass
+
+@router.get('/{task_id}')
+async def task_by_id(task_id: int):
+    pass
+
+@router.post('/create')
+async def create_task():
+    pass
+
+@router.put('/update/{task_id}')
+async def update_task(task_id: int):
+    pass
+
+@router.delete('/delete/{task_id}')
+async def delete_task(task_id: int):
+    pass
