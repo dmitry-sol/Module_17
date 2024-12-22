806b3d06cdef2a1f4a659ddc74439b1289b2e543
dmitry-sol <dmitry-sol@mail.ru>
2024-12-23 02:06:38 +1100

    Delete app/create_tables.py

--- app/create_tables.py
+++
@@ -1,6 +0,0 @@
-from app.backend.db import engine, Base
-from app.models.task import Task
-from app.models.user import User
-
-Task.__table__.create(engine, checkfirst=True)
-User.__table__.create(engine, checkfirst=True)
